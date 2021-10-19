812383b2803961750c13eb58d52cd47573e37b78
Carlos Del Carpio <dangdude14@yahoo.com>
2021-10-18 20:55:10 -0400

    Created new files, pushing all firebase updates

---
+++ 5102 Swift Games/GameBoard.swift
@@ -0,0 +1,12 @@
+//
+//  GameBoard.swift
+//  5102 Swift Games
+//
+//  Created by Carlos Del Carpio on 10/18/21.
+//
+
+import Foundation
+
+class GameBoard {
+    var board : [Int]?
+}
---
+++ 5102 Swift Games/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  5102 Swift Games
+//
+//  Created by Carlos Del Carpio on 10/18/21.
+//
+
+import Foundation
+
+class User {
+    var identifier : String?
+    var token : String?
+}
